1481e2e22e54068970fe476ea772e5bd5551d11b
Martin <martdang5@gmail.com>
2023-10-01 18:03:18 -0400

    Coded it, did the css, and got the functionality for the js, with fetching api, adding queryselectors, and calling the function in which fetchapi is located

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Weather-App
 Weather app that displays weather info based on given location
+23c5cd760af54f9e864215602233009
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 
-let city = "Canada"; //replace with textContent
+//Query Selectors
+let city = document.querySelector('.search'); //class
+let temp = document.querySelector('#temperature');
+let humidity = document.querySelector('#humidity');
+let windspeed = document.querySelector('#windspeed');
+let cityText = document.querySelector('.city-text');
+let cloudy = document.querySelector('#cloudy');
 
-fetch('http://api.weatherapi.com/v1/current.json?key=23c5cd760af54f9e864215602233009&q=' + city + '@aqi=no')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        let Country = data.location.country;
-        console.log(Country);
-    })
+
+function fetchWeather(city) { //Parameter city passed in, so the api knows what city to get info for
+        fetch('https://api.weatherapi.com/v1/current.json?key=23c5cd760af54f9e864215602233009&q=' + city)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            cityText.textContent = 'Weather in ' + data.location.name;
+            temp.textContent = data.current.temp_f + ' F';
+            cloudy.textContent = data.current.condition.text;
+            humidity.textContent = 'Humdidity: ' + data.current.humidity;
+            windspeed.textContent = 'Windspeed: ' + data.current.wind_mph + 'mph';
+        })
+}
+
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', () => {
+    fetchWeather(city.value);
+}) //city.value is the input put in by user, which is the city as a parameter
+
+
+
+
 
 
